2dde5ec43dba09a22ee8ccc7e9e8a130e4497eb3
ramesh-bse522 <105699109+ramesh-bse522@users.noreply.github.com>
2022-05-17 23:04:51 -0700

    Update RewardsCustomerServiceImpl.java

--- src/main/java/com/example/rewards/service/impl/RewardsCustomerServiceImpl.java
+++ src/main/java/com/example/rewards/service/impl/RewardsCustomerServiceImpl.java
@@ -2,6 +2,8 @@ package com.example.rewards.service.impl;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,16 +13,23 @@ import com.example.rewards.service.RewardsCustomerService;
 
 @Service
 public class RewardsCustomerServiceImpl implements RewardsCustomerService{
-	
+    private static final Logger log = LoggerFactory.getLogger(RewardsCustomerServiceImpl.class);
+
 	@Autowired
 	RewardsCustomerRepo rewardsCustomerRepo;
 
 	@Override
-	public RewardsCustomer getRewardsCustomer(String custId) {	
+	public RewardsCustomer getRewardsCustomer(String custId) {
+		log.debug("Entered in RewardsCustomerServiceImpl getRewardsCustomer method for custId :{"+custId+"}");
 		Optional<RewardsCustomer> optRewardsCustomer=rewardsCustomerRepo.findById(custId);
-		if(optRewardsCustomer==null|| !optRewardsCustomer.isPresent()) 
+		if(optRewardsCustomer==null|| !optRewardsCustomer.isPresent()) {
+			log.warn("Invalid Customer for custId :{"+custId+"}");
 			return null;
+		}
+		log.info("Valid Customer for custId :{"+custId+"}");
+
 		return optRewardsCustomer.get();
 	}
 
 }
+
